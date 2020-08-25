fcf0a50eb1242397ce11e2db7330fa3960960b09
the_sogafoi <seyedalirezaghoreishi1994@gmail.com>
2020-08-25 15:17:55 +0430

    seshanbe

---
+++ components/ChooseWorkGroup.vue
@@ -0,0 +1,80 @@
+<template>
+  <v-card :loading="isUpdating">
+    <v-form>
+      <v-container>
+        <v-row>
+          <v-col cols="12">
+            <v-autocomplete
+              v-model="selected"
+              :disabled="isUpdating"
+              :items="workGroups"
+              filled
+              chips
+              label="Select"
+              item-text="title"
+              item-value="id"
+              multiple
+            >
+              <template v-slot:selection="data">
+                <v-chip
+                  v-bind="data.attrs"
+                  :input-value="data.selected"
+                  close
+                  @click="data.select"
+                  @click:close="remove(data.item)"
+                >{{ data.item.title }}</v-chip>
+              </template>
+            </v-autocomplete>
+          </v-col>
+        </v-row>
+      </v-container>
+    </v-form>
+  </v-card>
+</template>
+
+<script>
+export default {
+  props: ["work_groups"],
+  data() {
+    return {
+      selected: [],
+      isUpdating: false,
+      workGroups: [],
+    };
+  },
+  created() {
+    this.fixWokrGroupData();
+  },
+  watch: {
+    selected() {
+      this.$emit("selected_work_group_changed", this.selected);
+    },
+    isUpdating(val) {
+      if (val) {
+        setTimeout(() => (this.isUpdating = false), 3000);
+      }
+    },
+  },
+
+  methods: {
+    fixWokrGroupData() {
+      for (let i = 0; i < this.work_groups.length; i++) {
+        if (i != 0) {
+          //   this.workGroups.push({ divider: true });
+        }
+        console.log(this.work_groups[i]);
+        this.workGroups.push({ header: this.work_groups[i].header });
+        if (this.work_groups[i].children.length != 0) {
+          this.workGroups.push(...this.work_groups[i].children);
+        }
+      }
+    },
+    remove(item) {
+      const index = this.selected.indexOf(item);
+      this.selected = this.selected.filter((workGroup) => {
+        return workGroup != item.id;
+      });
+    },
+  },
+};
+</script>
--- store/index.js
+++ store/index.js
@@ -3,137 +3,157 @@ export const strict = false
 export const state = () => ({
   baseUrl: 'http://api.aradmobile.matican.work/api/',
   cityList: [{
-      name: "آذربایجان شرقی",
-      id: 1,
-    },
-    {
-      name: "آذربایجان غربی",
-      id: 2,
-    },
-    {
-      name: "اردبیل",
-      id: 3,
-    },
-    {
-      name: "اصفهان",
-      id: 4,
-    },
-    {
-      name: "بوشهر",
-      id: 5,
-    },
-    {
-      name: "تهران",
-      id: 6,
-    },
-    {
-      name: "چهارمحال و بختیاری",
-      id: 7,
-    },
-    {
-      name: "خراسان جنوبی",
-      id: 8,
-    },
-    {
-      name: "خراسان رضوی",
-      id: 9,
-    },
-    {
-      name: "خراسان شمالی",
-      id: 10,
-    },
-    {
-      name: "خوزستان",
-      id: 11,
-    },
-    {
-      name: "زنجان",
-      id: 12,
-    },
-    {
-      name: "سمنان",
-      id: 13,
-    },
-    {
-      name: "سیستان و بلوچستان",
-      id: 14,
-    },
-    {
-      name: "فارس",
-      id: 15,
-    },
-    {
-      name: "قم",
-      id: 16,
-    },
-    {
-      name: "کرمانشاه",
-      id: 17,
-    },
-    {
-      name: "کرمان",
-      id: 18,
-    },
-    {
-      name: "کهکیلویه و بویراحمد",
-      id: 19,
-    },
-    {
-      name: "گلستان",
-      id: 20,
-    },
-    {
-      name: "گیلان",
-      id: 21,
-    },
-    {
-      name: "کرمان",
-      id: 22,
-    },
-    {
-      name: "لرستان",
-      id: 23,
-    },
-    {
-      name: "مازندران",
-      id: 24,
-    },
-    {
-      name: "هرمزگان",
-      id: 25,
-    },
-    {
-      name: "مرکزی",
-      id: 26,
-    },
-    {
-      name: "همدان",
-      id: 27,
-    },
-    {
-      name: "یزد",
-      id: 28,
-    },
-    {
-      name: "البرز",
-      id: 29,
-    },
-    {
-      name: "ایلام",
-      id: 30,
-    },
-    {
-      name: "قزوین",
-      id: 31,
-    },
+    name: "آذربایجان شرقی",
+    id: 1,
+  },
+  {
+    name: "آذربایجان غربی",
+    id: 2,
+  },
+  {
+    name: "اردبیل",
+    id: 3,
+  },
+  {
+    name: "اصفهان",
+    id: 4,
+  },
+  {
+    name: "بوشهر",
+    id: 5,
+  },
+  {
+    name: "تهران",
+    id: 6,
+  },
+  {
+    name: "چهارمحال و بختیاری",
+    id: 7,
+  },
+  {
+    name: "خراسان جنوبی",
+    id: 8,
+  },
+  {
+    name: "خراسان رضوی",
+    id: 9,
+  },
+  {
+    name: "خراسان شمالی",
+    id: 10,
+  },
+  {
+    name: "خوزستان",
+    id: 11,
+  },
+  {
+    name: "زنجان",
+    id: 12,
+  },
+  {
+    name: "سمنان",
+    id: 13,
+  },
+  {
+    name: "سیستان و بلوچستان",
+    id: 14,
+  },
+  {
+    name: "فارس",
+    id: 15,
+  },
+  {
+    name: "قم",
+    id: 16,
+  },
+  {
+    name: "کرمانشاه",
+    id: 17,
+  },
+  {
+    name: "کرمان",
+    id: 18,
+  },
+  {
+    name: "کهکیلویه و بویراحمد",
+    id: 19,
+  },
+  {
+    name: "گلستان",
+    id: 20,
+  },
+  {
+    name: "گیلان",
+    id: 21,
+  },
+  {
+    name: "کرمان",
+    id: 22,
+  },
+  {
+    name: "لرستان",
+    id: 23,
+  },
+  {
+    name: "مازندران",
+    id: 24,
+  },
+  {
+    name: "هرمزگان",
+    id: 25,
+  },
+  {
+    name: "مرکزی",
+    id: 26,
+  },
+  {
+    name: "همدان",
+    id: 27,
+  },
+  {
+    name: "یزد",
+    id: 28,
+  },
+  {
+    name: "البرز",
+    id: 29,
+  },
+  {
+    name: "ایلام",
+    id: 30,
+  },
+  {
+    name: "قزوین",
+    id: 31,
+  },
   ],
   required: [(v) => !!v || 'This Field Is Required'],
+
+  work_groups: []
 
 })
 export const getters = {
   isAuthenticated(state) {
     return state.token != null
+  },
+  workGroups(state) {
+    return state.work_groups;
   }
 }
 
-export const mutations = {}
+export const mutations = {
+  SET_WORK_GROUPS(state, work_groups) {
+    state.work_groups = work_groups;
+  }
+}
+
+
+export const actions = {
+  async refreshWorkGroups({ commit }) {
+    let { data } = await this.$axios.$get('workgroup/component/index')
+    commit('SET_WORK_GROUPS', data);
+  },
+  async nuxtServerInit({ dispatch }) {
+    await dispatch('refreshWorkGroups')
+  },
+}
