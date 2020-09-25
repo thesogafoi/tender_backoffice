33c5f3fb8d8fdd3667c981a1860a9fb49428ad62
the_sogafoi <seyedalirezaghoreishi1994@gmail.com>
2020-09-25 20:18:03 +0330

    Jome 8

--- nuxt.config.js
+++ nuxt.config.js
@@ -87,7 +87,7 @@ export default {
    ** See https://axios.nuxtjs.org/options
    */
   axios: {
-    baseURL: "http://tender-backend.localhost/api/"
+    baseURL: "http://asan.matican.work/api/"
   },
   /*
    ** vuetify module configuration
