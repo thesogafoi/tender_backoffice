e7e63d228e2e5075834f459de882e621069512b1
the_sogafoi <seyedalirezaghoreishi1994@gmail.com>
2020-09-26 16:50:20 +0330

    shanbe 6

--- nuxt.config.js
+++ nuxt.config.js
@@ -2,6 +2,14 @@ import colors from "vuetify/es5/util/colors";
 
 export default {
 
+  /*
+     ** Nuxt rendering mode
+     ** See https://nuxtjs.org/api/configuration-mode
+     */
+  /*
+   ** Nuxt rendering mode
+   ** See https://nuxtjs.org/api/configuration-mode
+   */
 
   /*
    ** Nuxt rendering mode
